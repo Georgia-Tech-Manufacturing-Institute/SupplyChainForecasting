2c9794cb1037130fc6a157e19b4c5abf71f748f1
nvanhand <73389036+nvanhand@users.noreply.github.com>
2026-06-14 19:47:47 -0400

    plotly, tohtml, part_number filter

--- app/reporting/data_quality.py
+++ app/reporting/data_quality.py
@@ -1,6 +1,18 @@
 from app.core.loader import *
 from app.reporting.core import * 
-def coverage_report(wf, cf):
+from app.prefixe import pre, dirs
+
+def coverage_report(since_idx=None, until_idx=None, part_prefix=None):
+    wf, cf = get_wf_cf()
+    if since_idx is not None:
+        wf = wf[wf['orderidx'] >= since_idx]
+        cf = cf[cf['orderidx'] >= since_idx]
+    if until_idx is not None:
+        wf = wf[wf['orderidx'] <= until_idx]
+        cf = cf[cf['orderidx'] <= until_idx]
+    if part_prefix:
+        wf = wf[wf['part'].str.startswith(part_prefix)]
+        cf = cf[cf['part'].str.startswith(part_prefix)]
     # PNs that are in consumption but NEVER found in a waterfall report
     # no_existing_wf = cf_pns - wf_pns
     # # PNs that are in waterfall but never consumed in 
@@ -13,8 +25,10 @@ def coverage_report(wf, cf):
     # wf_set = set(wf[['part', oi]].itertuples(index=False, name=None))
 
     existing_pred = wf[['part', 'orderidx']].drop_duplicates()
+    existing_pred = existing_pred[existing_pred!=0]
     existing_pred['value'] = -1
     existing_orders = cf[['part', 'orderidx']].drop_duplicates()
+    existing_orders = existing_orders[existing_orders!=0]
     existing_orders['value'] = 1
 
     result = (
@@ -22,23 +36,47 @@ def coverage_report(wf, cf):
         .groupby(['part', 'orderidx'], as_index=False)['value']
         .sum()
     )
-
     pivot = result.pivot(index='part', columns='orderidx')
-    import plotly.graph_objects as go
-
+    print(pivot.columns)
+    z = pivot.values.copy()
+    z[z == 1] = 2
+    z[z == 0] = 1
+    z[z == -1] = 0
     fig = go.Figure(
-        data=go.Heatmap(
-            z=pivot.values,
+        go.Heatmap(
+            z=z,
             x=pivot.columns,
             y=pivot.index,
-            colorscale='RdBu',
-            zmid=0
+            zmin=0, zmax=2,
+            colorscale=[
+                [0.0, "#2166ac"],
+                [0.3333, "#2166ac"],
+
+                [0.3333, "#2de05a"],
+                [0.6666, "#2de05a"],
+
+                [0.6666, "#b2182b"],
+                [1.0, "#b2182b"],
+            ],
+            colorbar=dict(
+                orientation='h',
+                title='',
+                tickvals=[0, 1, 2],
+                ticktext=["-1", "0", "1"]
+            )
         )
+    )    
+    fig.update_layout(
+        width=800,
+        margin=dict(t=30),
+        height=1000
+    )
+    fig.update_xaxes(
+        fixedrange=True,
+        ticklabelposition="outside top",
+        side="top"
     )
 
-    fig.update_layout(
-        title="Part vs IDX alignment heatmap",
-        yaxis_scaleanchor="x",
-        height=800,
-    )
-    # fig.show(renderer='browser')
+    return pio.to_html(fig, full_html=False, include_plotlyjs="cdn")
+
+   
